93529b3646a35adbc38e6adeffc5ff1ce8709380
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2026-05-19 16:38:20 -0500

    fix errors

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -35,11 +35,8 @@ urlpatterns = [
         "failures/",
         include(("api.v1.failures.urls", "failures"), namespace="failures"),
     ),
-<<<<<<< HEAD
-=======
     path(
         "dashboard/",
         include(("api.v1.dashboard.urls", "dashboard"), namespace="dashboard"),
     ),
->>>>>>> origin/main
 ]
